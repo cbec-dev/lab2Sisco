25ffb6a35cbe8f9b17f9d4457d0bf2b03b2a7ac1
Powei <cristobal.becerraf@usach.cl>
2018-07-30 18:32:27 -0400

    encriptacion y desencriptacion simple

--- lab2.py
+++ lab2.py
@@ -4,6 +4,24 @@
 
 #Functions
 
+def arrayAString(arr):
+    return ''.join(arr)
+
+def encriptar(llave, mensaje):
+    msj_enc = ''
+    for i in range(len(mensaje)):
+        llave_c = ord(llave[i % len(llave)])
+        mensaje_c = ord(mensaje[i % len(mensaje)])
+        msj_enc += chr((llave_c + mensaje_c) % 127)
+    return msj_enc
+
+def desencriptar(llave, msjEncriptado):
+    mensaje = []
+    for i, c in enumerate(msjEncriptado):
+        llave_c = ord(llave[i % len(llave)])
+        enc_c = ord(c)
+        mensaje.append(chr((enc_c - llave_c) % 127))
+    return arrayAString(mensaje)
 
 
 #Main
@@ -11,3 +29,14 @@ if __name__ == '__main__':
 
     #Code
     
+    mensaje = "Pantalones de queso"
+    llave = "SISCO"
+
+    msj_enc = encriptar(llave, mensaje)
+    msj_dec = desencriptar(llave, msj_enc)
+
+    print('mensaje: ' + mensaje)
+    print('llave: ' + llave)
+    print('mensaje encriptado: ' + msj_enc)
+    print('mensaje desencriptado: ' + msj_dec)
+
