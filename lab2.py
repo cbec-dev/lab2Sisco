f51bf1e50e33eb9169a12252ccbd8e0135bf4d8c
Powei <cristobal.becerraf@usach.cl>
2018-07-31 01:08:49 -0400

    algoritmo ahora funciona por bloques

--- lab2.py
+++ lab2.py
@@ -1,13 +1,17 @@
 #Imports
 
-
+abc = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
+bloque_size = 2
 
 #Functions
 
 def arrayAString(arr):
     return ''.join(arr)
 
-def encriptar(llave, mensaje):
+
+
+
+def enc(llave, mensaje):
     msj_enc = ''
     for i in range(len(mensaje)):
         llave_c = ord(llave[i % len(llave)])
@@ -15,7 +19,7 @@ def encriptar(llave, mensaje):
         msj_enc += chr((llave_c + mensaje_c) % 127)
     return msj_enc
 
-def desencriptar(llave, msjEncriptado):
+def dec(llave, msjEncriptado):
     mensaje = []
     for i, c in enumerate(msjEncriptado):
         llave_c = ord(llave[i % len(llave)])
@@ -24,19 +28,42 @@ def desencriptar(llave, msjEncriptado):
     return arrayAString(mensaje)
 
 
+def algoritmo_enc(llave, msj):
+    msj_enc = ''
+    for i in xrange(0, len(msj), bloque_size):
+        bloque = msj[i:i+bloque_size]
+        msj_enc = msj_enc + enc(llave, bloque)
+    return msj_enc
+
+def algoritmo_dec(llave, msj_enc):
+    msj_dec = ''
+    for i in xrange(0, len(msj_enc), bloque_size):
+        bloque = msj_enc[i:i+bloque_size]
+        msj_dec = msj_dec + dec(llave, bloque)
+    return msj_dec
+
+    
+
+
 #Main
 if __name__ == '__main__':
 
     #Code
     
-    mensaje = "Pantalones de queso"
-    llave = "SISCO"
+    mensaje = "Hola, esto es una prueba de encr"
+    llave = "sisco"
 
-    msj_enc = encriptar(llave, mensaje)
-    msj_dec = desencriptar(llave, msj_enc)
+    msj_enc = algoritmo_enc(llave, mensaje)
+    msj_dec = algoritmo_dec(llave, msj_enc)
 
+
+    print('Bloque: ' + str(bloque_size) + " bytes")
+    print('alfabeto: '+ '\"' + abc + '\"')
     print('mensaje: ' + mensaje)
     print('llave: ' + llave)
     print('mensaje encriptado: ' + msj_enc)
     print('mensaje desencriptado: ' + msj_dec)
+
+    
+
 
