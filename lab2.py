9fd17dcd36c4539e2eba325d7499c9e30e964593
Powei <cristobal.becerraf@usach.cl>
2018-07-31 13:55:20 -0400

    ahora pide mensaje, llave y tamaño bloque por consola

--- lab2.py
+++ lab2.py
@@ -1,14 +1,11 @@
 #Imports
 
 abc = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-bloque_size = 2
 
 #Functions
 
 def arrayAString(arr):
     return ''.join(arr)
-
-
 
 
 def enc(llave, mensaje):
@@ -28,14 +25,14 @@ def dec(llave, msjEncriptado):
     return arrayAString(mensaje)
 
 
-def algoritmo_enc(llave, msj):
+def algoritmo_enc(llave, msj, bloque_size):
     msj_enc = ''
     for i in xrange(0, len(msj), bloque_size):
         bloque = msj[i:i+bloque_size]
         msj_enc = msj_enc + enc(llave, bloque)
     return msj_enc
 
-def algoritmo_dec(llave, msj_enc):
+def algoritmo_dec(llave, msj_enc, bloque_size):
     msj_dec = ''
     for i in xrange(0, len(msj_enc), bloque_size):
         bloque = msj_enc[i:i+bloque_size]
@@ -50,15 +47,20 @@ if __name__ == '__main__':
 
     #Code
     
-    mensaje = "Hola, esto es una prueba de encr"
-    llave = "sisco"
+    mensaje = raw_input('Ingrese mensaje a encriptar: ')
+    llave = raw_input('Ingrese llave: ')
+    bloque_size = int(raw_input('Ingrese largo de bloque en bytes: '))
+    try:
+        number = int(bloque_size)
+    except ValueError:
+        print("Ingrese un largo de bloque valido")
 
-    msj_enc = algoritmo_enc(llave, mensaje)
-    msj_dec = algoritmo_dec(llave, msj_enc)
+    msj_enc = algoritmo_enc(llave, mensaje, bloque_size)
+    msj_dec = algoritmo_dec(llave, msj_enc, bloque_size)
 
 
+    print('')
     print('Bloque: ' + str(bloque_size) + " bytes")
-    print('alfabeto: '+ '\"' + abc + '\"')
     print('mensaje: ' + mensaje)
     print('llave: ' + llave)
     print('mensaje encriptado: ' + msj_enc)
